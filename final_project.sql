79bca2d0d53c42e13437cee8defb0f235f071a52
mahanor <107750019+mahanor@users.noreply.github.com>
2022-12-15 11:28:45 +0530

    Add files via upload
    Uploaded sucessfully

---
+++ final_project.sql
@@ -0,0 +1,5 @@
+
+use final_project;
+use final_project;
+INSERT INTO `user` VALUES 
+(1,1,'admin@gmail.com','Kiran','Mahanor','$2a$10$uA7FXFWM7Mf2vVm2/EABZeW7lDkITMuzBSCY3VGliJlkAJ7OCb8D.',current_timestamp(),'ADMIN');
